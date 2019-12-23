826d74844d02d743b07af7e3ca3eae12017759e4
kim joon <minziappa@gmail.com>
2019-12-24 00:38:14 +0900

    Build number 20191224

--- ssh/releaseTomcatStaging.sh
+++ ssh/releaseTomcatStaging.sh
@@ -10,7 +10,7 @@ APP_PATH_FROM_WAR="/home/joonk/git/abilists_client/build/libs/ROOT.war"
 
 USER_ID=joonk
 
-M_VERSION="0.4.1"
+M_VERSION="0.5.0"
 
 build_fuc () { 
     printf "Starting base_bean."
